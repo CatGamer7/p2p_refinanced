d408acbb81eadf69376b904f353f1a358758992a
Exeskicks <danil.yudin.2014@list.ru>
2024-06-26 17:04:47 +0700

    Initialization project add libraries

---
+++ src/main/java/com/example/finance/p2p_financeFriend/P2pFinanceFriendApplication.java
@@ -0,0 +1,13 @@
+package com.example.finance.p2p_financeFriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pFinanceFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2pFinanceFriendApplication.class, args);
+	}
+
+}
